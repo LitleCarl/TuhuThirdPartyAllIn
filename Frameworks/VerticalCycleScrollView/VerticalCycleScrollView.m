c546af0e9f8af0c7c2ea81bf87fabeb90043b881
Tsao <Tsao@AppledeMacBook-Pro.local>
2016-07-31 21:23:18 +0800

    first commit

---
+++ Frameworks/VerticalCycleScrollView/VerticalCycleScrollView.m
@@ -0,0 +1,176 @@
+
+
+#import "VerticalCycleScrollView.h"
+#import "NSTimer+Addition.h"
+
+@interface VerticalCycleScrollView () <UIScrollViewDelegate>
+
+@property (nonatomic , assign) NSInteger currentPageIndex;
+@property (nonatomic , assign) NSInteger totalPageCount;
+@property (nonatomic , strong) NSMutableArray *contentViews;
+@property (nonatomic , strong) UIScrollView *scrollView;
+
+@property (nonatomic , strong) NSTimer *animationTimer;
+@property (nonatomic , assign) NSTimeInterval animationDuration;
+
+@end
+
+@implementation VerticalCycleScrollView
+
+- (void)setTotalPagesCount:(NSInteger (^)(void))totalPagesCount
+{
+    _totalPageCount = totalPagesCount();
+    if (_totalPageCount > 0) {
+        [self configContentViews];
+        [self.animationTimer resumeTimerAfterTimeInterval:self.animationDuration];
+    }
+}
+
+- (id)initWithFrame:(CGRect)frame animationDuration:(NSTimeInterval)animationDuration
+{
+    self = [self initWithFrame:frame];
+    if (animationDuration > 0.0) {
+        self.animationTimer = [NSTimer scheduledTimerWithTimeInterval:(self.animationDuration = animationDuration)
+                                                               target:self
+                                                             selector:@selector(animationTimerDidFired:)
+                                                             userInfo:nil
+                                                              repeats:YES];
+        [self.animationTimer pauseTimer];
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.autoresizesSubviews = YES;
+        self.scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
+        self.scrollView.autoresizingMask = 0xFF;
+        self.scrollView.contentMode = UIViewContentModeCenter;
+        self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.scrollView.frame), 3*CGRectGetHeight(self.scrollView.frame));
+        self.scrollView.delegate = self;
+        self.scrollView.contentOffset = CGPointMake(0, CGRectGetHeight(self.scrollView.frame));
+        self.scrollView.pagingEnabled = YES;
+        [self addSubview:self.scrollView];
+        self.currentPageIndex = 0;
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark - 私有函数
+
+- (void)configContentViews
+{
+    [self.scrollView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    [self setScrollViewContentDataSource];
+    
+    NSInteger counter = 0;
+    for (UIView *contentView in self.contentViews) {
+        contentView.userInteractionEnabled = YES;
+        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(contentViewTapAction:)];
+        [contentView addGestureRecognizer:tapGesture];
+        CGRect rightRect = contentView.frame;
+        rightRect.origin = CGPointMake(0, CGRectGetHeight(self.scrollView.frame) * (counter ++));
+        
+        contentView.frame = rightRect;
+        [self.scrollView addSubview:contentView];
+    }
+    [_scrollView setContentOffset:CGPointMake(0, _scrollView.frame.size.height)];
+}
+
+/**
+ *  设置scrollView的content数据源，即contentViews
+ */
+- (void)setScrollViewContentDataSource
+{
+    NSInteger previousPageIndex = [self getValidNextPageIndexWithPageIndex:self.currentPageIndex - 1];
+    NSInteger rearPageIndex = [self getValidNextPageIndexWithPageIndex:self.currentPageIndex + 1];
+    if (self.contentViews == nil) {
+        self.contentViews = [@[] mutableCopy];
+    }
+    [self.contentViews removeAllObjects];
+    
+    if (self.fetchContentViewAtIndex) {
+        [self.contentViews addObject:self.fetchContentViewAtIndex(previousPageIndex)];
+        [self.contentViews addObject:self.fetchContentViewAtIndex(_currentPageIndex)];
+        [self.contentViews addObject:self.fetchContentViewAtIndex(rearPageIndex)];
+    }
+}
+
+- (NSInteger)getValidNextPageIndexWithPageIndex:(NSInteger)currentPageIndex;
+{
+    if(currentPageIndex == -1) {
+        return self.totalPageCount - 1;
+    } else if (currentPageIndex == self.totalPageCount) {
+        return 0;
+    } else {
+        return currentPageIndex;
+    }
+}
+
+#pragma mark -
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self.animationTimer pauseTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self.animationTimer resumeTimerAfterTimeInterval:self.animationDuration];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    int contentOffsetY = scrollView.contentOffset.y;
+    if(contentOffsetY >= (2 * CGRectGetHeight(scrollView.frame))) {
+        self.currentPageIndex = [self getValidNextPageIndexWithPageIndex:self.currentPageIndex + 1];
+       
+        
+        //NSLog(@"next，当前页:%d",self.currentPageIndex);
+        [self configContentViews];
+    }
+    if(contentOffsetY <= 0) {
+        self.currentPageIndex = [self getValidNextPageIndexWithPageIndex:self.currentPageIndex - 1];
+       
+        
+        //NSLog(@"previous，当前页:%d",self.currentPageIndex);
+        [self configContentViews];
+    }
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    [scrollView setContentOffset:CGPointMake(0, CGRectGetHeight(scrollView.frame)) animated:YES];
+}
+
+#pragma mark -
+#pragma mark - 响应事件
+
+- (void)animationTimerDidFired:(NSTimer *)timer
+{
+    CGPoint newOffset = CGPointMake(self.scrollView.contentOffset.x, self.scrollView.contentOffset.y + CGRectGetHeight(self.scrollView.frame));
+    [self.scrollView setContentOffset:newOffset animated:YES];
+}
+
+- (void)contentViewTapAction:(UITapGestureRecognizer *)tap
+{
+    if (self.TapActionBlock) {
+        self.TapActionBlock(self.currentPageIndex);
+    }
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
